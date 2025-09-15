0898a4fb70ae61e21e26e93673fd0f262851524c
gun1977 <nakata1977@ucomp.co.kr>
2025-09-15 11:01:46 +0900

    페이지 추가

--- assets/guide/js/include.js
+++ assets/guide/js/include.js
@@ -302,47 +302,47 @@ const headerNavigation = function (useFullLink) {
 				<div class="g-sub-body">
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">멤버십​</span></p>
-						${createMenuLink("product/FO_Y_BA_001.html", useFullLink)}
+						${createMenuLink("benefit/FO_Y_BA_001.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">멤버십 (멤버십 상세 레이어)</span></p>
-						${createMenuLink("product/FO_Y_BA_002_L.html", useFullLink)}
+						${createMenuLink("benefit/FO_Y_BA_002_L.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">멤버십 (멤버십 상세 레이어)</span></p>
-						${createMenuLink("product/FO_Y_BA_002_L.html", useFullLink)}
+						${createMenuLink("benefit/FO_Y_BA_002_L.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">제휴카드</span></p>
-						${createMenuLink("product/FO_Y_BB_001.html", useFullLink)}
+						${createMenuLink("benefit/FO_Y_BB_001.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">제휴카드 (상세 레이어)</span></p>
-						${createMenuLink("product/FO_Y_BB_002_L.html", useFullLink)}
+						${createMenuLink("benefit/FO_Y_BB_002_L.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">이벤트​</span></p>
-						${createMenuLink("product/FO_Y_BC_001.html", useFullLink)}
+						${createMenuLink("benefit/FO_Y_BC_001.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">이벤트 상세</span></p>
-						${createMenuLink("product/FO_Y_BC_002.html", useFullLink)}
+						${createMenuLink("benefit/FO_Y_BC_002.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">부가서비스​</span></p>
-						${createMenuLink("product/FO_Y_BD_001.html", useFullLink)}
+						${createMenuLink("benefit/FO_Y_BD_001.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">온라인 법률상담 목록</span></p>
-						${createMenuLink("product/FO_Y_BD_002.html", useFullLink)}
+						${createMenuLink("benefit/FO_Y_BD_002.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">온라인 법률 상담 등록 or 상세</span></p>
-						${createMenuLink("product/FO_Y_BD_003_L.html", useFullLink)}
+						${createMenuLink("benefit/FO_Y_BD_003_L.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">온라인 법률 상담 예시 보기​</span></p>
-						${createMenuLink("product/FO_Y_BD_004_L.html", useFullLink)}
+						${createMenuLink("benefit/FO_Y_BD_004_L.html", useFullLink)}
 					</dl>
 				</div>
 			</li>
@@ -355,61 +355,61 @@ const headerNavigation = function (useFullLink) {
 				<div class="g-sub-body">
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">메인</span></p>
-						${createMenuLink("product/FO_Y_CA_001.html", useFullLink)}
+						${createMenuLink("my/FO_Y_CA_001.html", useFullLink)}
 					</dl>
 				</div>
 				<div class="g-sub-body">
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">계약 정보 목록</span></p>
-						${createMenuLink("product/FO_Y_YB_001.html", useFullLink)}
+						${createMenuLink("my/FO_Y_YB_001.html", useFullLink)}
 					</dl>
 				</div>
 				<div class="g-sub-body">
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">계약 형황</span></p>
-						${createMenuLink("product/FO_Y_YB_002.html", useFullLink)}
+						${createMenuLink("my/FO_Y_YB_002.html", useFullLink)}
 					</dl>
 				</div>
 				<div class="g-sub-body">
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">계약 정보</span></p>
-						${createMenuLink("product/FO_Y_YB_002.html", useFullLink)}
+						${createMenuLink("my/FO_Y_YB_002.html", useFullLink)}
 					</dl>
 				</div>
 				<div class="g-sub-body">
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">납입현황</span></p>
-						${createMenuLink("product/FO_Y_YC_001.html", useFullLink)}
+						${createMenuLink("my/FO_Y_YC_001.html", useFullLink)}
 					</dl>
 				</div>
 				<div class="g-sub-body">
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">증명서 발급 목록</span></p>
-						${createMenuLink("product/FO_Y_YD_001_L.html", useFullLink)}
+						${createMenuLink("my/FO_Y_YD_001_L.html", useFullLink)}
 					</dl>
 				</div>
 				<div class="g-sub-body">
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">멤버십 카드</span></p>
-						${createMenuLink("product/FO_Y_YE_001_L.html", useFullLink)}
+						${createMenuLink("my/FO_Y_YE_001_L.html", useFullLink)}
 					</dl>
 				</div>
 				<div class="g-sub-body">
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">결제 정보 변경</span></p>
-						${createMenuLink("product/FO_Y_YF_001.html", useFullLink)}
+						${createMenuLink("my/FO_Y_YF_001.html", useFullLink)}
 					</dl>
 				</div>
 				<div class="g-sub-body">
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">개인 정보 변경</span></p>
-						${createMenuLink("product/FO_Y_YF_002.html", useFullLink)}
+						${createMenuLink("my/FO_Y_YF_002.html", useFullLink)}
 					</dl>
 				</div>
 				<div class="g-sub-body">
 					<dl class="g-sub-wrap" data-status="">
 						<p class="g-sub-title"><span class="em">개인 정보 변경</span></p>
-						${createMenuLink("product/FO_Y_YF_002.html", useFullLink)}
+						${createMenuLink("my/FO_Y_YF_002.html", useFullLink)}
 					</dl>
 				</div>
 			</li>
