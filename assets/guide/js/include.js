a207ff3f074b5bb20eae1409a80329e80a363e03
gun1977 <nakata1977@ucomp.co.kr>
2025-10-20 18:25:17 +0900

    수정

--- assets/guide/js/include.js
+++ assets/guide/js/include.js
@@ -479,10 +479,12 @@ const headerNavigation = function (useFullLink) {
 					</dl>
 					<dl class="g-sub-wrap" data-status="완료">
 						<p class="g-sub-title"><span class="em">[디자인필요] - 고객 칭찬 후기 > 내가 칭찬한 글 > 칭찬한 글 없는 경우(10p)</span></p>
+						<p class="g-sub-text"><span class="em">아이콘 필요</span></p>
 						${createMenuLink("cs/FO_Y_CC_001_nodata.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="완료">
 						<p class="g-sub-title"><span class="em">[피그마] - ing - 공지사항 No Data</span></p>
+						<p class="g-sub-text"><span class="em">아이콘 필요</span></p>
 						${createMenuLink("cs/FO_Y_CE_001_nodata.html", useFullLink)}
 					</dl>
 				</div>
