4003c72aa614e2c4d3e1f58d052cc4082ce6691a
gun1977 <nakata1977@ucomp.co.kr>
2025-10-20 21:51:14 +0900

    수정

--- assets/guide/js/include.js
+++ assets/guide/js/include.js
@@ -130,12 +130,10 @@ const headerNavigation = function (useFullLink) {
 					</div>
 					<div class="g-sub-wrap" data-status="완료">
 						<p class="g-sub-title"><span class="em">[피그마] - ing - 알럿</span></p>
-						<p class="g-sub-text"><span class="em">아이콘 필요</span></p>
 						${createMenuLink("common/alert.html", useFullLink)}
 					</div>
 					<div class="g-sub-wrap" data-status="완료">
 						<p class="g-sub-title"><span class="em">[피그마] - ing - 토스트</span></p>
-						<p class="g-sub-text"><span class="em">아이콘 필요</span></p>
 						${createMenuLink("common/toast.html", useFullLink)}
 					</div>
 					<div class="g-sub-wrap" data-status="">
@@ -144,14 +142,17 @@ const headerNavigation = function (useFullLink) {
 					</div>
 					<div class="g-sub-wrap" data-status="완료">
 						<p class="g-sub-title"><span class="em">8차 - 시스템 점검</span></p>
+						<p class="g-sub-text"><span class="em">아이콘 필요</span></p>
 						${createMenuLink("common/FO_Y_ZA_001.html", useFullLink)}
 					</div>
 					<div class="g-sub-wrap" data-status="완료">
 						<p class="g-sub-title"><span class="em">8차 - 404 에러</span></p>
+						<p class="g-sub-text"><span class="em">아이콘 필요</span></p>
 						${createMenuLink("common/FO_Y_ZA_002.html", useFullLink)}
 					</div>
 					<div class="g-sub-wrap" data-status="완료">
 						<p class="g-sub-title"><span class="em">8차 - 505 에러</span></p>
+						<p class="g-sub-text"><span class="em">아이콘 필요</span></p>
 						${createMenuLink("common/FO_Y_ZA_003.html", useFullLink)}
 					</div>
 					<div class="g-sub-wrap" data-status="완료">
