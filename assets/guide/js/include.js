d18e72318a700a2b53d414c96b51feebb0bf780c
gun1977 <nakata1977@ucomp.co.kr>
2025-10-28 19:56:55 +0900

    컨텐츠 적용

--- assets/guide/js/include.js
+++ assets/guide/js/include.js
@@ -355,7 +355,6 @@ const headerNavigation = function (useFullLink) {
 					</dl>
 					<dl class="g-sub-wrap" data-status="완료">
 						<p class="g-sub-title"><span class="em">Step05. 계약완료* > 개인정보 제3자 제공 동의 (18p)</span></p>
-						<p class="g-sub-text" style="color: red;"><span class="em">컨텐츠 작업 필요</span></p>
 						${createMenuLink("direct/FO_Y_DH_001_L.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="완료">
