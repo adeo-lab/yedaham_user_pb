5fc6ac232ff9c4c0e9eea339360d7f5a868e1816
Baek Sujin <whitesj121@gmail.com>
2025-11-13 09:58:45 +0900

    퀵메뉴 테스트

--- assets/guide/js/include.js
+++ assets/guide/js/include.js
@@ -338,6 +338,7 @@ const headerNavigation = function (useFullLink) {
 					<dl class="g-sub-wrap" data-status="완료">
 						<p class="g-sub-title"><span class="em">Step01. 상품선택 (일시납)</span></p>
 						<p class="g-sub-text"><span class="em">모바일 레이어 : 윤슬 4호에 적용</span></p>
+						<p class="g-sub-edit"><span class="em">주석: 1113 작업</span></p>
 						${createMenuLink("direct/FO_Y_DA_001_03.html", useFullLink)}
 					</dl>
 					<dl class="g-sub-wrap" data-status="완료">
