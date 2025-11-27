860cbcdcfd180cfde8ced2a5e69489f696e2bb51
Baek Sujin <whitesj121@gmail.com>
2025-11-27 17:58:31 +0900

    lenis 적용

--- assets/front/js/ui.main_corp.js
+++ assets/front/js/ui.main_corp.js
@@ -37,6 +37,22 @@ function initCompanyMain() {
     scroller: document.body,
     markers: false,
   });
+
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smoothWheel: true,
+    smoothTouch: false,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    ScrollTrigger.update(); 
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
+
+  lenis.on('scroll', ScrollTrigger.update);
 }
 
 
